d8b6949a3ffe3012a2c6d551625628c53f848d26
JGCoder96 <jgcoder96@gmail.com>
2024-10-18 11:11:31 -0400

    NextUiI config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@nextui-org/react";
+
 export const App = () => (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div className="flex gap-4 items-center">
+    <Button size="sm">Small</Button>
+    <Button size="md">Medium</Button>
+    <Button size="lg">Large</Button>
+  </div>
 );
